4d3438b0e9bb8447afd5643c5548840f81c42591
diogorodriguees <diogorodrigueslife@gmail.com>
2023-04-29 13:53:57 -0300

    :rocket: nova maneira de exibir o historico

--- src/components/Resumo/Resumo.tsx
+++ src/components/Resumo/Resumo.tsx
@@ -4,33 +4,17 @@ import classes from "./Resumo.module.css";
 import Item from "../Item";
 import api from "../../api/api";
 
-type EntradaSaidaType = {
+type historicoItems = {
     descricao: String;
     valor: number;
-    tipo: string
+    tipo: string;
 };
 
-function Resumo() {
-    const [entradasMaisSaidas, setEntradasMaisSaidas] = useState<EntradaSaidaType[]>([]);
+type historico = {
+    historico: historicoItems[];
+};
 
-    const buscarHistorico = async () => {
-        const { data } = await api.get("historico");
-
-        setEntradasMaisSaidas(data);
-    };
-
-    const gerarHistorico = async () => {
-
-    };
-
-    useEffect(() => {
-        buscarHistorico();
-    }, []);
-
-    useEffect(() => {
-        gerarHistorico();
-    }, [entradasMaisSaidas]);
-
+function Resumo({ historico }: historico) {
     return (
         <div className={classes.resumo}>
             <div className={classes.titulo_colunas}>
@@ -39,11 +23,18 @@ function Resumo() {
                 <p className={classes.tipo}>Tipo</p>
             </div>
             <div>
-                {entradasMaisSaidas.map((item, index) =>{
-                    if(index >= 6){
-                        return
+                {historico.map((item, index) => {
+                    if (index >= 6) {
+                        return;
                     }
-                   return (<Item descricao={item.descricao} valor={item.valor} tipo={item.tipo} key={index}/>)
+                    return (
+                        <Item
+                            descricao={item.descricao}
+                            valor={item.valor}
+                            tipo={item.tipo}
+                            key={index}
+                        />
+                    );
                 })}
             </div>
         </div>
